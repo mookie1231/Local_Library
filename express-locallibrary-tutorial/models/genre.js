1dbfdd7ea999c7213d911384787fe4450a2895dc
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-03-16 18:45:07 -0400

    database has been populated

---
+++ express-locallibrary-tutorial/models/genre.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const genreSchema = new Schema ({
+    name: {type: String, minLength: 3, maxLength: 100 }
+});
+
+genreSchema.virtual("url").get(function () {
+    return `/catalog/genre/${this._id}`;
+});
+//what is a virtual - getting a URL access for each of the entries? 
+
+module.exports = mongoose.model("Genre", genreSchema);
+
+
+
