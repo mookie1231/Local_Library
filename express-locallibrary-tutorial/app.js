bc7e4706684a409e644694c8bde7d6029d83696d
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-03-21 07:18:11 -0400

    routes and controllers complete

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -7,12 +7,14 @@ const mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
