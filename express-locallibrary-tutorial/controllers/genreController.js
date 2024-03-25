2c3973727fbfff15e7dfbf7f7350a95690847136
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-03-25 10:21:20 -0400

    genre_list

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -1,9 +1,11 @@
+const { render } = require("pug");
 const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+  const allGenres = await Genre.find().sort({first_name : 1}).exec();
+  res.render ("genre_list", {title: "Genre List", genre_list: allGenres}); 
 });
 
 // Display detail page for a specific Genre.
