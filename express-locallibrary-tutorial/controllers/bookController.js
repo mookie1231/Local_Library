98f6918e7ed4442e2903a79f9a2bb70abe590661
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-03-24 20:07:07 -0400

    controller changes

--- express-locallibrary-tutorial/controllers/bookController.js
+++ express-locallibrary-tutorial/controllers/bookController.js
@@ -1,4 +1,8 @@
 const Book = require("../models/book");
+const Author = require("../models/author");
+const Genre = require("../models/genre");
+const BookInstance = require("../models/bookinstance");
+
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
@@ -7,7 +11,28 @@ exports.index = asyncHandler(async (req, res, next) => {
 
 // Display list of all books.
 exports.book_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book list");
+    const [
+      numBooks,
+      numBookInstances,
+      numAvailableBookInstances,
+      numAuthors,
+      numGenres,
+    ] = await Promise.all([
+      Book.countDocuments({}).exec(),
+      BookInstance.countDocuments({}).exec(),
+      BookInstance.countDocuments({ status: "Available" }).exec(),
+      Author.countDocuments({}).exec(),
+      Genre.countDocuments({}).exec(),
+    ]);
+
+    res.render("index", {
+      title: "Local Library Home",
+      book_count: numBooks,
+      book_instance_count: numBookInstances,
+      book_instance_available_count: numAvailableBookInstances,
+      author_count: numAuthors,
+      genre_count: numGenres,
+  });
 });
 
 // Display detail page for a specific book.
@@ -44,3 +69,4 @@ exports.book_update_get = asyncHandler(async (req, res, next) => {
 exports.book_update_post = asyncHandler(async (req, res, next) => {
   res.send("NOT IMPLEMENTED: Book update POST");
 });
+
