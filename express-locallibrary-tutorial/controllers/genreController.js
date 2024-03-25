79ca1d75d6952c9dc0eefe57171763bd920e00ba
Mukesh Loganathan <mukeshloganathan12@gmail.com>
2024-03-25 12:08:19 -0400

    information about the books

--- express-locallibrary-tutorial/controllers/genreController.js
+++ express-locallibrary-tutorial/controllers/genreController.js
@@ -1,7 +1,7 @@
 const { render } = require("pug");
 const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
-const Book = require.apply("../models/book");
+const Book = require("../models/book");
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
